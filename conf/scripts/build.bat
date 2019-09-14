50896aa49d0d5c6b34f2dc4823ad9c51f68b6410
ayoubensalem <ayoubensalem@gmail.com>
2019-09-15 00:13:00 +0100

    add scripts

---
+++ conf/scripts/build.bat
@@ -0,0 +1,11 @@
+set pathMSBuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+set pathNuget="C:\jenkins\nuget.exe"
+set solutionPath=%1
+
+echo "pathMSBuild: %pathMSBuild%"
+echo "pathNuget: %pathMSBuild%"
+echo "solutionPath: %solutionPath%"
+
+%pathMSBuild% %solutionPath% /t:Clean /v:q /nologo
+%pathNuget% restore %solutionPath%
+%pathMSBuild% %solutionPath% /t:Build /p:Configuration=Debug /v:q /nologo
